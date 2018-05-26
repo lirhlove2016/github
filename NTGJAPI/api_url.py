62f10a242d32dc47752086ff898ac408800c0ac7
testtest <testing901206@gmail.com>
2018-05-26 17:46:11 +0800

    auto_test

---
+++ NTGJAPI/api_url.py
@@ -0,0 +1,19 @@
+# -*- coding:utf8 -*-
+
+# ------------------------------ admin login url     ---------------------
+ADMIN_API = {
+    "fsdregister":"/flyHandApp/api/user/loginTest",
+    "fsduserpassword":"/flyHandApp/api/user/loginAdd",
+    "fsdlogin":"/flyHandApp/api/user/loginPassword",
+    "fsdcheckaccount":"/flyHandApp/api/user/checkAccountRegisted",
+    "fsdsms":"/flyHandApp/api/user/getSmsCheckCode?phone=test",
+    "addtools":"/flyHandApp/api/user/addTool/v1?userId=id&token=TOKEN",
+    "syslogin":"/management/sys/login",
+    "freezeflyaccount":"/management/user/updateFlyUserAccountState",
+    }
+
+
+
+
+
+
